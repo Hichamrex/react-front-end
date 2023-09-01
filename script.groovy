d494669b51274cffc6ec90fd49b11bb2d1c301a3
Hicham LAQRAFI <53710462+Hichamrex@users.noreply.github.com>
2023-09-01 02:42:16 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -18,13 +18,23 @@ def dockerPush(String imageName) {
 }
 
 def deployApplication(String imageName) {
-    echo "Deploying docker image to EC2"
-    def shellCmd = "bash ./server-script.sh $imageName"
-    sshagent(['ec2-server-key']) {
-        sh "scp ./server-script.sh ec2-user@15.188.59.125:/home/ec2-user"
-        sh "scp ./docker-compose.yaml ec2-user@15.188.59.125:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@15.188.59.125 ${shellCmd}"
+    echo "Deploying the application to EC2..."
+    def shellCmd = "bash ./server-script-back.sh $imageName"
+     withCredentials([usernamePassword(credentialsId: 'azure-vm', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./server-script-back.sh $USERNAME@20.166.72.53:/home/$USERNAME"
+        sh "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./docker-compose-spring.yaml $USERNAME@20.166.72.53:/home/$USERNAME"
+        sh "echo $PASSWORD | sshpass --password-stdin ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $USERNAME@20.166.72.53 ${shellCmd}"
     }
 }
+
+// def deployApplication(String imageName) {
+//     echo "Deploying docker image to EC2"
+//     def shellCmd = "bash ./server-script.sh $imageName"
+//     sshagent(['ec2-server-key']) {
+//         sh "scp ./server-script.sh ec2-user@15.188.59.125:/home/ec2-user"
+//         sh "scp ./docker-compose.yaml ec2-user@15.188.59.125:/home/ec2-user"
+//         sh "ssh -o StrictHostKeyChecking=no ec2-user@15.188.59.125 ${shellCmd}"
+//     }
+// }
 
 return this
